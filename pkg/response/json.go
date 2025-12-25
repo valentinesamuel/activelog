52cc1d849a29a99b148d0d6f05dcf814a9530b07
valentinesamuel <valentinesamuel2580@gmail.com>
2025-12-25 11:15:29 +0100

    feat: created reusable response and error writers

--- pkg/response/json.go
+++ pkg/response/json.go
@@ -1,1 +1,18 @@
 package response
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func sendJSON(w http.ResponseWriter, statusCode int, data interface{}) error {
+	w.Header().Set("Content Type", "application/json")
+	w.WriteHeader(statusCode)
+	return json.NewEncoder(w).Encode(data)
+}
+
+func Error(w http.ResponseWriter, statusCode int, message string) error {
+	return sendJSON(w, statusCode, map[string]string{
+		"error": message,
+	})
+}
