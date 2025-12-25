371e06786ae7a37ffcb63b05c8e3bec4123dfe98
valentinesamuel <valentinesamuel2580@gmail.com>
2025-12-25 11:11:00 +0100

    feat: added mux router

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,19 +4,35 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/valentinesamuel/activelog/internal/handlers"
 )
 
 func main() {
 	fmt.Println("Starting ActiveLog API...")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	router := mux.NewRouter()
+
+	healthHandler := handlers.NewHealthHandler()
+
+	router.Handle("/health", healthHandler).Methods("GET")
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.Write([]byte(`{"message":"Welcome to ActiveLog API"}`))
 
-		log.Println("Server starting on :8080")
-	})
-	
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
+	}).Methods("GET")
+
+	log.Println("Server starting on :8080")
+
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Header().Set("Content-Type", "application/json")
+	// 	w.Write([]byte(`{"message":"Welcome to ActiveLog API"}`))
+	// if err := http.ListenAndServe(":8080", nil); err != nil {
+	// 	log.Fatal(err)
+	// }
+	// })
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+
 }
