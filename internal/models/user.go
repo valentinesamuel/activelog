ae72482470db5b3178900a03a015971c20203a7d
valentinesamuel <valentinesamuel2580@gmail.com>
2025-12-29 12:13:12 +0100

    feat: added password hash migration

--- internal/models/user.go
+++ internal/models/user.go
@@ -2,8 +2,9 @@ package models
 
 type User struct {
 	BaseEntity
-	Email    string `json:"email" db:"email"`
-	Username string `json:"username" db:"username"`
+	Email        string `json:"email" `
+	Username     string `json:"username" `
+	PasswordHash string `json:"password_hash" `
 
 	Activities []Activity `json:"activities,omitempty"`
 }
